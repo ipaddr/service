e1d6bd308abf6d64467677e3b6de642960c0fbd4
Iip Permana <uipaddr@gmail.com>
2023-03-16 10:10:38 +0700

    done service tutorial

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { AccountService } from '../shared/account.service';
 
 @Component({
   selector: 'app-account',
@@ -8,11 +9,11 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 export class AccountComponent {
   @Input() account: {name: string, status: string};
   @Input() id: number;
-  @Output() statusChanged = new EventEmitter<{id: number, newStatus: string}>();
 
+  constructor(private accountService: AccountService){}
 
   onSetTo(status: string) {
-    this.statusChanged.emit({id: this.id, newStatus: status});
-    console.log('A server status changed, new status: ' + status);
+    this.accountService.updateStatus(this.id, status);
+    this.accountService.statusUpdateEvent.emit(status);
   }
 }
